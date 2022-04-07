3a50f5c1afa6e64b988cf844f4107249269aa4f3
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-07 15:22:49 -0300

    Creation of Spring+Angular project.

---
+++ src/test/java/user/com/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package user.com.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
