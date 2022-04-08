342c625e34d7a3668edb735f9b092284ecea1322
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-08 10:55:35 -0300

    Add repository's entities.

---
+++ src/main/java/user/com/ecommerce/repository/IProductRepository.java
@@ -0,0 +1,8 @@
+package user.com.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import user.com.ecommerce.model.entity.Product;
+
+public interface IProductRepository extends JpaRepository<Product,Long> {
+
+}
