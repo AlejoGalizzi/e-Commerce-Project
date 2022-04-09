a2cd93a770eee492b659473f8f0c3c6f78630448
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-09 12:21:23 -0300

    Repositories changed.

--- src/main/java/user/com/ecommerce/repository/ICategoryRepository.java
+++ src/main/java/user/com/ecommerce/repository/ICategoryRepository.java
@@ -1,8 +1,11 @@
 package user.com.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import user.com.ecommerce.model.entity.Category;
 
+@RepositoryRestResource(collectionResourceRel = "category",path = "product-category")
 public interface ICategoryRepository extends JpaRepository<Category,Long> {
 
+  Category findByName(String name);
 }
--- src/main/java/user/com/ecommerce/repository/IProductRepository.java
+++ src/main/java/user/com/ecommerce/repository/IProductRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import user.com.ecommerce.model.entity.Product;
 
 public interface IProductRepository extends JpaRepository<Product,Long> {
-
 }
