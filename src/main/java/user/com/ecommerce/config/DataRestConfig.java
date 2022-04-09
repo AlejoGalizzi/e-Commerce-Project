ace766d481feda501df918c5beb9b6e3e2b16a2d
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-09 12:30:46 -0300

    Endpoint's configuration added.

--- src/main/java/user/com/ecommerce/config/DataRestConfig.java
+++ src/main/java/user/com/ecommerce/config/DataRestConfig.java
@@ -1,5 +1,24 @@
 package user.com.ecommerce.config;
 
-public class DataRestConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import user.com.ecommerce.model.entity.Product;
 
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+  @Override
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+      CorsRegistry cors) {
+
+    HttpMethod[] unsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+
+    config.getExposureConfiguration()
+        .forDomainType(Product.class)
+        .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+        .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+  }
 }
