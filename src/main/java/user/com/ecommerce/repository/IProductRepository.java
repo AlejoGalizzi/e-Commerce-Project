62032b472e07cec335c5c5ab1bde9f8359f90b03
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-11 18:43:50 -0300

    Add Cross Origin to the front end side.

--- src/main/java/user/com/ecommerce/repository/IProductRepository.java
+++ src/main/java/user/com/ecommerce/repository/IProductRepository.java
@@ -1,7 +1,9 @@
 package user.com.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import user.com.ecommerce.model.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface IProductRepository extends JpaRepository<Product,Long> {
 }
