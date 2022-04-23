0a956c587b077bf667bd401cabb93331cccef991
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-23 01:21:24 -0300

    e-Commerce FrontEnd: Add event listeners to cart status component to update price and quantity.

--- FrontEnd/src/app/components/product-list/product-list.component.ts
+++ FrontEnd/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -20,7 +22,7 @@ export class ProductListComponent implements OnInit {
 
 
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -80,7 +82,10 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(product: Product) {
-    console.log(`Adding to cart: ${product.name} , ${product.unitPrice}`);
+
+    const cartItem: CartItem = new CartItem(product);
+
+    this.cartService.addToCart(cartItem);
 
   }
 }
