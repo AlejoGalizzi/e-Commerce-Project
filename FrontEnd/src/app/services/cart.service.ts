d4db90343558550f1dd306b6ee5c824cfd42bdd9
Alejo Galizzi <alejogalizzi@gmail.com>
2022-04-23 00:54:43 -0300

    e-Commerce FrontEnd: Add logic to set total price and total quantity of a shop.

--- FrontEnd/src/app/services/cart.service.ts
+++ FrontEnd/src/app/services/cart.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
   constructor() { }
+
+  addToCart(cartItem: CartItem){
+    let alreadyExistInCart:boolean = false;
+    let existingCartItem: CartItem = undefined!;
+
+    for (let item of this.cartItems) {
+      if(item.id === cartItem.id){
+        existingCartItem = item;
+        break;
+      }
+    }
+    alreadyExistInCart = (existingCartItem != undefined);
+    if(alreadyExistInCart){
+      existingCartItem.quantity++;
+    }else{
+      this.cartItems.push(cartItem);
+    }
+    this.computeCartTotals();
+  }
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+    for (let item of this.cartItems) {
+      totalPriceValue += item.unitPrice*item.quantity;
+      totalQuantityValue += item.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  }
 }
