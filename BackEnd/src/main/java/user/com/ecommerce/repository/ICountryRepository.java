a929ee8c20aee7831b3d11ac62b71f8a2a8557a6
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-11 12:45:53 -0300

    e-Commerce BackEnd: Add country and state repository.

---
+++ BackEnd/src/main/java/user/com/ecommerce/repository/ICountryRepository.java
@@ -0,0 +1,12 @@
+package user.com.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import user.com.ecommerce.model.entity.Country;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface ICountryRepository extends JpaRepository<Country, Long> {
+
+}
