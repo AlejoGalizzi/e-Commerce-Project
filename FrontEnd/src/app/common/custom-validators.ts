b48a8b3e04cd682d0ee2455005bfd66cb2bb9260
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-12 12:25:33 -0300

    e-Commerce FrontEnd: Change custom validator class name.

--- FrontEnd/src/app/common/custom-validators.ts
+++ FrontEnd/src/app/common/custom-validators.ts
@@ -1,6 +1,6 @@
 import { FormGroup, ValidationErrors } from "@angular/forms";
 
-export class Validators {
+export class CustomValidators {
   static notOnlyWhiteSpace(control: FormGroup): ValidationErrors {
     if((control.value != null) && (control.value.trim().length === 0)) {
       return {'notOnlyWhiteSpace': true};
