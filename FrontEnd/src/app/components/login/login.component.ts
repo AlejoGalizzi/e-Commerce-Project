1306860f23a97f842b00165aef96cbc0fefad09f
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-17 13:03:38 -0300

    e-Commerce FrontEnd: Fix some errors with okta-angular.

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { OktaAuthService } from '@okta/okta-angular';
-import OktaSignIn from '@okta/okta-signin-widget';
+import { Component,Inject, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+import * as OktaSignIn from '@okta/okta-signin-widget';
+
 import myAppConfig from 'src/app/config/my-app-config';
 
 @Component({
@@ -12,7 +16,8 @@ export class LoginComponent implements OnInit {
 
   oktaSignIn: any;
 
-  constructor(private oktaAuthService: OktaAuthService) {
+  constructor(private oktaAuthService: OktaAuthStateService,
+     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
     this.oktaSignIn = new OktaSignIn({
       logo: 'assets/images/logo.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
@@ -32,7 +37,7 @@ export class LoginComponent implements OnInit {
       el: '#okta-sign-in-widget'},
       (response)=>{
         if (response.status === 'SUCCESS'){
-          this.oktaAuthService.signInWithRedirect();
+          this.oktaAuth.signInWithRedirect();
         }
       },
       (error)=> {
