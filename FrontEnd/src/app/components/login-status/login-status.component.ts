09b9dafd0fe8b69bc27df359ca5996376b214b2b
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-18 19:44:39 -0300

    e-Commerce FrontEnd: Add a session storage when you logged in to save the email of the current user.

--- FrontEnd/src/app/components/login-status/login-status.component.ts
+++ FrontEnd/src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,7 @@ export class LoginStatusComponent implements OnInit {
 
   isAuthenticated: boolean = false;
   userFullName: string;
+  storage: Storage = sessionStorage;
 
   constructor(private oktaAuthService: OktaAuthStateService,
     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
@@ -29,6 +30,10 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         res => {
           this.userFullName = res.name;
+
+          const email = res.email;
+
+          this.storage.setItem('userEmail',JSON.stringify(email))
         }
       )
     }
