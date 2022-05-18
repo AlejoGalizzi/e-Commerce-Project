6ad49af793787b207153c8fa6b07e2754e958971
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-18 19:29:10 -0300

    e-Commerce BackEnd: Remove unused imports.

--- BackEnd/src/main/java/user/com/ecommerce/repository/ICategoryRepository.java
+++ BackEnd/src/main/java/user/com/ecommerce/repository/ICategoryRepository.java
@@ -2,7 +2,6 @@ package user.com.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import user.com.ecommerce.model.entity.Category;
 
 @RepositoryRestResource(collectionResourceRel = "categories", path = "product-categories")
--- BackEnd/src/main/java/user/com/ecommerce/repository/IStateRepository.java
+++ BackEnd/src/main/java/user/com/ecommerce/repository/IStateRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import user.com.ecommerce.model.entity.State;
 
 @RepositoryRestResource(collectionResourceRel = "states", path = "states")
