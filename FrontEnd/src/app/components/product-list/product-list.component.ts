8fd93a44e6554fa3c933afb4cf608791dc5d6bdd
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-18 18:55:34 -0300

    e-Commerce FrontEnd: Fix problem to show alert message when there are any products.

--- FrontEnd/src/app/components/product-list/product-list.component.ts
+++ FrontEnd/src/app/components/product-list/product-list.component.ts
@@ -12,7 +12,7 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
+  products: Product[] = [];
   previouseKeywordSearched: string;
   currentCategoryId: number = 1;
   previousCategoryId: number = 1;
