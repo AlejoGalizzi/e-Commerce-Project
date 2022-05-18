d7df9813f09d039b262e074cef58ad9b44470c2f
Alejo Galizzi <alejogalizzi@gmail.com>
2022-05-18 19:43:47 -0300

    e-Commerce FrontEnd: Add the order history service.

---
+++ FrontEnd/src/app/services/order-history.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../common/order-history';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  orderHistoryUrl: string = "http://localhost:4200/api/orders";
+  
+  constructor(private httpClient: HttpClient) { }
+
+  getOrderHistory(email: string): Observable<GetResponseOrderHistory> {
+    const url = `${this.orderHistoryUrl}/search/findByCustomerEmail?email=${email}`;
+
+    return this.httpClient.get<GetResponseOrderHistory>(url);
+  }
+}
+
+interface GetResponseOrderHistory {
+  _embedded: {
+    orders: OrderHistory[];
+  }
+}
